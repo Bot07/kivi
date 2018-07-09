4d06998dd9658c1b831c8c27fe90593928b066ed
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-09 17:50:27 +0300

    Update k.js

--- k.js
+++ k.js
@@ -1,6 +1,7 @@
 const embed = require('discord.js'); 
 const Discord = require('discord.js'); 
-const client = new Discord.Client(); 
+const client = new Discord.Client();
+const collector = new Discord.MessageCollector();
 var prefix = 'k!' 
 client.on("ready", () => {client.user.setGame('k!yardım | Oralet <3 | Sunucu Sayısı: ' + client.guilds.size , 'https://twitch.tv/GardropFuat') 
 }); 
