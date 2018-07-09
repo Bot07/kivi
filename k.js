23cb699931d19ff1b483ead771a26e316c544943
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-09 17:39:23 +0300

    Update k.js

--- k.js
+++ k.js
@@ -40,5 +40,16 @@ let embed = new Discord.RichEmbed()
 msg.channel.send({embed: embed})  
 } 
 });
-
+if (message.content === prefix + 'oraletiç'){
+        message.channel.send("Kivi mi? Oralet mi?");
+        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        console.log(collector)
+        collector.on('collect', message => {
+            if (message.content == "Kivi") {
+                message.channel.send("a");
+            } else if (message.content == "Oralet") {
+                message.channel.send("b");
+            }
+        })
+});
 client.login(process.env.BOT_TOKEN)
